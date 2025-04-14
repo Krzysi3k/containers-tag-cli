aebf5437bec3993da09fab1d1f3e3a007800baa5
Krzysi3k <global.takeover@o2.pl>
2025-04-14 13:35:11 +0200

    changed question flow

---
+++ _question_flow.py
@@ -0,0 +1,53 @@
+from InquirerPy import prompt
+from dataclasses import dataclass
+
+
+
+@dataclass
+class ImageTag:
+    image_name: str
+    tags: list[str]
+    current_tag: str
+
+
+class QuestionFlow:
+    
+    def __init__(self, image_tags: list[ImageTag]):
+        questions = [
+        {
+            "message": "select image",
+            "type": "fuzzy",
+            "choices": [ t.image_name for t in image_tags ],
+            "max_height": "30%",
+            "name": "image_step"
+        },
+        {
+            "message": "select new tag",
+            "type": "fuzzy",
+            "choices": lambda result: [ tag for i in image_tags if i.image_name == result["image_step"] for tag in i.tags ],
+            "multiselect": False,
+            "max_height": "30%",
+            "name": "tag_step",
+        },
+        {
+            "message": "apply new tag to .env file?",
+            "type": "confirm",
+            "default": True,
+            "name": "apply_step",
+        },
+        {
+            "message": "choose stack",
+            "type": "list",
+            "choices": ["homestack","chatops","wireguard","go back", "cancel"],
+            "name": "stack_step",
+            "default": "cancel",
+            "when": lambda result: result["apply_step"]
+        }]
+        result = prompt(
+            questions,
+            style={"questionmark": "#ff9d00 bold"},
+            vi_mode=True,
+            style_override=False,
+        )
+        _ = [ result.update({'current_tag': i.current_tag}) for i in image_tags if result['image_step'] == i.image_name ]
+        self.result = result
--- question_flow.py
+++ question_flow.py
@@ -1,7 +1,5 @@
-from InquirerPy import prompt
+from InquirerPy import inquirer
 from dataclasses import dataclass
-
-
 
 @dataclass
 class ImageTag:
@@ -9,45 +7,50 @@ class ImageTag:
     tags: list[str]
     current_tag: str
 
-
 class QuestionFlow:
     
     def __init__(self, image_tags: list[ImageTag]):
-        questions = [
-        {
-            "message": "select image",
-            "type": "fuzzy",
-            "choices": [ t.image_name for t in image_tags ],
-            "max_height": "30%",
-            "name": "image_step"
-        },
-        {
-            "message": "select new tag",
-            "type": "fuzzy",
-            "choices": lambda result: [ tag for i in image_tags if i.image_name == result["image_step"] for tag in i.tags ],
-            "multiselect": False,
-            "max_height": "30%",
-            "name": "tag_step",
-        },
-        {
-            "message": "apply new tag to .env file?",
-            "type": "confirm",
-            "default": True,
-            "name": "apply_step",
-        },
-        {
-            "message": "choose stack",
-            "type": "list",
-            "choices": ["homestack","chatops","wireguard","go back", "cancel"],
-            "name": "stack_step",
-            "default": "cancel",
-            "when": lambda result: result["apply_step"]
-        }]
-        result = prompt(
-            questions,
-            style={"questionmark": "#ff9d00 bold"},
-            vi_mode=True,
-            style_override=False,
-        )
-        _ = [ result.update({'current_tag': i.current_tag}) for i in image_tags if result['image_step'] == i.image_name ]
-        self.result = result
+        # Get image selection
+        image_step = inquirer.fuzzy(
+            message="select image",
+            choices=[t.image_name for t in image_tags],
+            max_height="30%"
+        ).execute()
+        
+        # Get tag selection
+        selected_image_tags = [tag for i in image_tags if i.image_name == image_step for tag in i.tags]
+        tag_step = inquirer.fuzzy(
+            message="select new tag",
+            choices=selected_image_tags,
+            multiselect=False,
+            max_height="30%"
+        ).execute()
+        
+        # Apply tag confirmation
+        apply_step = inquirer.confirm(
+            message="apply new tag to .env file?",
+            default=True
+        ).execute()
+        
+        # Stack selection if applying
+        stack_step = None
+        if apply_step:
+            stack_step = inquirer.select(
+                message="choose stack",
+                choices=["homestack", "chatops", "wireguard", "go back", "cancel"],
+                default="cancel"
+            ).execute()
+        
+        # Construct result dictionary
+        self.result = {
+            "image_step": image_step,
+            "tag_step": tag_step,
+            "apply_step": apply_step,
+            "stack_step": stack_step
+        }
+        
+        # Add current_tag to result
+        for i in image_tags:
+            if self.result['image_step'] == i.image_name:
+                self.result['current_tag'] = i.current_tag
+                break
